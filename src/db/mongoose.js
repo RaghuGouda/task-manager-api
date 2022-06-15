a390755a06d3c295f8a5d0b1635cbe2a883ea1ec
Raghu Nagaraju <raghu.nagaraju.tv@vedantu.com>
2022-06-15 13:44:36 +0530

    init commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-mongoose.connect(`${process.env.MONGODB_URL}/task-manager-api`,{
+mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser:true
 })
 
